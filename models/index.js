b6274995a03646b7cb7cd8e642dde7323201673c
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-09 19:35:52 -0500

    updated models and one route

--- models/index.js
+++ models/index.js
@@ -5,13 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Driver, {
+  foreignKey: 'driver_id',
+});
 // Categories have many Products
-
+Reader.hasMany(Book, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Traveller.belongsToMany(Location, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  }
+  
+  
+});
 // Tags belongToMany Products (through ProductTag)
-
+Traveller.belongsToMany(Location, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false,
+    foreignKey: 'tag_id'
+  }
+  
+  
+});
 module.exports = {
   Product,
   Category,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [Category, {model: Tag, 
+        thorugh: ProductTag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      include: [Category, {model: Tag, 
+        thorugh: ProductTag}]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -25,6 +45,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
